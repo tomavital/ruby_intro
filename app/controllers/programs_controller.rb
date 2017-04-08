cf5f546f3526e07e7b223c578fdeff575cace205
rails -v <c:sites>
2017-04-08 13:18:29 -0500

    3

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -10,7 +10,16 @@ class ProgramsController < ApplicationController
   def first_program
     # Your code goes below.
 
-    @your_output = "Replace this string with your output"
+    the_number = rand(3)
+
+    if the_number < 1
+      @your_output = "Rock"
+    elsif the_number > 1
+      @your_output = "Paper"
+    else
+      @your_output = "Scissors"
+    end
+
 
     render("programs/first_program.html.erb")
   end
@@ -18,18 +27,32 @@ class ProgramsController < ApplicationController
   def second_program
     # Your code goes below.
 
-    @your_output = "Replace this string with your output"
+    our_numbers = [4, 10, 6]        # Create an array of numbers
+    squared_numbers = []            # Create an empty array
+
+    our_numbers.each do |num|       # For each element in numbers, (refer to it as "num")
+      square = num * num            # Square the number
+      squared_numbers.push(square)  # Push it into the squared_numbers array
+    end
+
+    @your_output = squared_numbers.sum  # Sum the squares
 
     render("programs/second_program.html.erb")
   end
 
   def third_program
     numbers = (1..999).to_a
+    good_numbers = []            # Create an empty array
 
+    numbers.each do |num|
+      if num%3 == 0 || num%5 == 0
+        good = num
+      good_numbers.push(good)  # Push it into the squared_numbers array
+    end
     # Your code goes below.
 
-    @your_output = "Replace this string with your output"
-
+    @your_output = good_numbers
+end
     render("programs/third_program.html.erb")
   end
 end
